ca9175abec32a186690918171fbede1211cf5909
diacarcor <diacarcor@gmail.com>
2021-05-25 17:34:20 +0200

    Implementation

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+from turtle import Turtle, Screen
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+color_list = [(249, 212, 93), (150, 69, 97), (53, 99, 155), (232, 137, 62), (107, 174, 211), (243, 237, 241), (114, 83, 59), (201, 146, 177), (200, 77, 109), (145, 134, 72), (230, 90, 59), (141, 192, 140), (72, 103, 90), (68, 162, 92), (5, 165, 179), (227, 161, 183), (115, 126, 142), (163, 196, 221), (16, 66, 123), (187, 24, 34), (13, 56, 103), (235, 172, 160), (175, 201, 179), (163, 200, 215), (186, 27, 25), (80, 55, 37), (96, 61, 30)]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+screen = Screen()
+screen.colormode(255)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Turtle setup
+paint = Turtle()
+paint.up()
+paint.speed("fastest")
+paint.hideturtle()
+# Initial position
+paint.setheading(225)
+paint.forward(300)
+paint.setheading(0)
+x_position = paint.xcor()
+y_position = paint.ycor()
+
+# Painting
+for y in range(10):
+    paint.setx(x_position)
+    paint.sety(y_position + y * 50)
+    for x in range(10):
+        paint.dot(20, random.choice(color_list))
+        paint.forward(50)
+
+
+screen.exitonclick()
